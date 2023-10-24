2316848d7214901a0b9b7ab4d10e2f9b870fd617
thedeviser22 <132663137+thedeviser22@users.noreply.github.com>
2023-10-24 12:11:42 +0100

    Add files via upload

---
+++ tic tac toe/src/Tictactoe.java
@@ -0,0 +1,82 @@
+import java.util.Scanner;
+
+public class Tictactoe {
+    public static void main(String[] args) {
+        char[][] board = {
+                {' ', ' ', ' '},
+                {' ', ' ', ' '},
+                {' ', ' ', ' '}
+        };
+        char currentPlayer = 'X';
+
+        boolean gameWon = false;
+        int moves = 0;
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            printBoard(board);
+            System.out.println("Player " + currentPlayer + ", enter row (0-2) and column (0-2) separated by a space: ");
+            int row = scanner.nextInt();
+            int col = scanner.nextInt();
+
+            if (row < 0 || row > 2 || col < 0 || col > 2 || board[row][col] != ' ') {
+                System.out.println("Invalid move. Try again.");
+                continue;
+            }
+
+            board[row][col] = currentPlayer;
+            moves++;
+
+            if (checkWin(board, currentPlayer)) {
+                printBoard(board);
+                System.out.println("Player " + currentPlayer + " wins!");
+                gameWon = true;
+                break;
+            } else if (moves == 9) {
+                printBoard(board);
+                System.out.println("It's a draw!");
+                break;
+            }
+
+            currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+        }
+
+        scanner.close();
+    }
+
+    public static void printBoard(char[][] board) {
+        System.out.println("  0 1 2");
+        for (int i = 0; i < 3; i++) {
+            System.out.print(i + " ");
+            for (int j = 0; j < 3; j++) {
+                System.out.print(board[i][j]);
+                if (j < 2) {
+                    System.out.print("|");
+                }
+            }
+            System.out.println();
+            if (i < 2) {
+                System.out.println("  -+-+-");
+            }
+        }
+    }
+
+    public static boolean checkWin(char[][] board, char player) {
+        for (int i = 0; i < 3; i++) {
+            if (board[i][0] == player && board[i][1] == player && board[i][2] == player) {
+                return true; // Check rows
+            }
+            if (board[0][i] == player && board[1][i] == player && board[2][i] == player) {
+                return true; // Check columns
+            }
+        }
+        if (board[0][0] == player && board[1][1] == player && board[2][2] == player) {
+            return true; // Check diagonals
+        }
+        if (board[0][2] == player && board[1][1] == player && board[2][0] == player) {
+            return true; // Check diagonals
+        }
+        return false;
+    }
+}
